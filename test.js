529b1105f387767f2c16abd81b923929cea7dec8
Tin Tran <tintran@Tins-MacBook-Pro.local>
2025-01-12 23:00:58 +0700

    init source

--- test.js
+++ test.js
@@ -2,61 +2,104 @@ const massive = require("massive");
 const { faker } = require("@faker-js/faker");
 
 // Number of employees to insert (can be dynamically provided)
-const numberOfEmployeesPerBatch = 5000;
-const numberOfBatches = 60;
-const getEmployees = (n) => {
-  const results = [];
-  for (let i = 0; i < n; i++) {
-    const firstName = faker.person.firstName();
-    const email = faker.internet.email();
-    const salary = faker.finance.amount(30000, 90000, 2); // Random salary between 30,000 and 90,000
-    const department = faker.commerce.department();
+// const numberOfEmployees = 5000;
+// const getEmployees = (n) => {
+//   const results = [];
+//   for (let i = 0; i < n; i++) {
+//     const firstName = faker.person.firstName();
+//     const email = faker.internet.email();
+//     const salary = faker.finance.amount(30000, 90000, 2); // Random salary between 30,000 and 90,000
+//     const department = faker.commerce.department();
 
-    // Create the insert promise for each employee
-    results.push({
-      first_name: firstName,
-      email: email,
-      salary: salary,
-      department: department,
-    });
-  }
-  return results;
-};
+//     // Create the insert data for each employee
+//     results.push({
+//       first_name: firstName,
+//       email: email,
+//       salary: salary,
+//       department: department,
+//     });
+//   }
+//   return results;
+// };
 
-// Connect to the PostgreSQL database
 massive({
   host: "localhost",
   port: 5432,
   database: "mydb",
   user: "tin",
   application_name: "nodejs",
-  poolSize: 90,
+  poolSize: 10,
   password: "tin",
 })
-  .then((db) => {
+  .then(async (db) => {
     console.log("Connected to database");
-    // Generate and insert employees dynamically
-    const batchInsertPromises = [];
-    const beforeInsert = performance.now();
-    for (let i = 0; i < numberOfBatches; i++) {
-      const payload = getEmployees(numberOfEmployeesPerBatch);
-      const batchInsertPromise = db.withTransaction(async (tx) => {
-        return await tx.employees.insert(payload);
-      });
-      batchInsertPromises.push(batchInsertPromise);
-    }
+    await db.query("truncate employees");
 
-    // Wait for all insertions to complete
-    Promise.all(batchInsertPromises)
-      .then((results) => {
-        console.log(`${
-          results.flatMap((a) => a).length
-        } employees inserted successfully. Took
-          ${(performance.now() - beforeInsert) / 1000} s`);
-      })
-      .catch((err) => {
-        console.error("Error inserting employees:", err);
-      });
+    const beforeInsert = performance.now();
+    const q1 = db.query(
+      `
+        INSERT INTO employees (first_name, email, salary, department)
+        VALUES
+        ('Jade', 'Jaunita.Wilderman47@gmail.com', '579.39', 'Health'), ('Cathy', 'Jon.Reynolds-Schamberger@hotmail.com', '421.93', 'Jewelry')
+        ON CONFLICT (first_name, department, email, salary) DO NOTHING
+        returning *;
+        select pg_sleep(15);
+
+        `
+    );
+    // this is to make sure that q1 is executed 1st
+    await new Promise((resolve, rej) => {
+      setTimeout(resolve, 0);
+    });
+
+    const q2 = db.query(
+      `
+        INSERT INTO employees (first_name, email, salary, department)
+        VALUES
+        ('Jade', 'Jaunita.Wilderman47@gmail.com', '579.39', 'Health22222'), ('Cathy', 'Jon.Reynolds-Schamberger@hotmail.com', '421.93', 'Jewelry')
+        ON CONFLICT (first_name, department, email, salary) DO NOTHING
+        returning *;
+        `
+    );
+    // select query does not need to wait
+    const q3 = db.query(
+      `
+         select * from employees
+        `
+    );
+
+    q1.then((result) => {
+      console.log("query 1", result);
+      console.log(
+        `First query inserted successfully. Took ${
+          (performance.now() - beforeInsert) / 1000
+        } s`
+      );
+    }).catch((err) => {
+      console.error("Error first query:", err);
+    });
+
+    q2.then((result) => {
+      console.log("query 2", result);
+      console.log(
+        `Second query inserted successfully. Took ${
+          (performance.now() - beforeInsert) / 1000
+        } s`
+      );
+    }).catch((err) => {
+      console.error("Error Second query:", err);
+    });
+
+    q3.then((result) => {
+      console.log("query 3", result);
+      console.log(
+        `3rd query selected successfully. Took ${
+          (performance.now() - beforeInsert) / 1000
+        } s`
+      );
+    }).catch((err) => {
+      console.error("Error 3rd query:", err);
+    });
   })
   .catch((err) => {
     console.error("Failed to connect to database", err);
