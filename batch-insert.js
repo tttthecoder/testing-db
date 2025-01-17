30f3a3c7eefb20224c69a5dd029adf4da4de1359
Tin Tran <tintran@Tins-MacBook-Pro.local>
2025-01-18 02:14:31 +0700

    init

---
+++ batch-insert.js
@@ -0,0 +1,106 @@
+const massive = require("massive");
+const { faker } = require("@faker-js/faker");
+
+// Number of event_stream records to insert per batch
+const numberOfRecordsPerBatch = 50000;
+const numberOfBatches = 10;
+
+// Function to generate event_stream records
+const getEventStreamRecords = (n) => {
+  const records = [];
+  for (let i = 0; i < n; i++) {
+    const profileId = faker.string.uuid(); // Random UUID for profile_id
+    const email = faker.internet.email(); // Random email
+    const phoneNumber = faker.phone.number(); // Random phone number
+    const accountId = faker.string.uuid(); // Random UUID for account_id
+    const brandId = faker.string.uuid(); // Random UUID for brand_id
+    const outletId = faker.string.uuid(); // Random UUID for outlet_id
+    const sourceId = faker.string.uuid(); // Random UUID for source_id
+    const sourceType = faker.helpers.arrayElement([
+      "CUSTOM_EVENT",
+      "SYSTEM_EVENT",
+    ]); // Random source type
+    const sourceCreatedAt = faker.date.recent(30).toISOString(); // Random recent date
+    const sourceUpdatedAt = faker.date
+      .between({ from: sourceCreatedAt, to: new Date() })
+      .toISOString(); // Random updated date
+    const data = { metadata: faker.lorem.paragraph() }; // Random JSON data
+
+    records.push({
+      profile_id: profileId,
+      email: email,
+      phone_number: phoneNumber,
+      account_id: accountId,
+      brand_id: brandId,
+      outlet_id: outletId,
+      source_id: sourceId,
+      source_type: sourceType,
+      source_created_at: sourceCreatedAt,
+      source_updated_at: sourceUpdatedAt,
+      data: data,
+    });
+  }
+  return records;
+};
+
+// Connect to the PostgreSQL database
+massive({
+  host: "localhost",
+  port: 5432,
+  database: "mydb",
+  user: "tin",
+  application_name: "nodejs",
+  poolSize: 90,
+  password: "tin",
+})
+  .then((db) => {
+    console.log("Connected to database");
+    // Generate and insert event_stream records dynamically
+    const batchInsertPromises = [];
+    const beforeInsert = performance.now();
+    for (let i = 0; i < numberOfBatches; i++) {
+      const payload = getEventStreamRecords(numberOfRecordsPerBatch);
+      const batchInsertPromise = db.withTransaction(async (tx) => {
+        const values = payload.map(
+          (item) =>
+            `('${item.profile_id}', '${item.email}', '${item.phone_number}', '${
+              item.account_id
+            }', '${item.brand_id}', '${item.outlet_id}', '${
+              item.source_id
+            }', '${item.source_type}', '${item.source_created_at}', '${
+              item.source_updated_at
+            }', '${JSON.stringify(item.data)}')`
+        );
+
+        const query = `
+          INSERT INTO public.event_stream (
+            profile_id, email, phone_number, account_id, brand_id, outlet_id, 
+            source_id, source_type, source_created_at, source_updated_at
+            , data
+          ) 
+          VALUES ${values.join(", ")}
+          ON CONFLICT ((md5(data::text)::uuid)) DO NOTHING
+          RETURNING *;
+          ;
+
+        `;
+
+        return await tx.query(query);
+      });
+      batchInsertPromises.push(batchInsertPromise);
+    }
+    // Wait for all insertions to complete
+    Promise.all(batchInsertPromises)
+      .then((results) => {
+        console.log(`${
+          results.flatMap((a) => a).length
+        } event_stream records inserted successfully. Took
+          ${(performance.now() - beforeInsert) / 1000} s`);
+      })
+      .catch((err) => {
+        console.error("Error inserting event_stream records:", err);
+      });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database", err);
+  });
--- init.sql
+++ init.sql
@@ -10,3 +10,86 @@ CREATE TABLE employees (
 
 -- Create a unique index to constrain uniqueness by first_name, department, email, and salary
 CREATE UNIQUE INDEX employees_unique_idx ON employees (first_name, department, email, salary);
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS public.event_stream
+(
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
+    profile_id text COLLATE pg_catalog."default",
+    email text COLLATE pg_catalog."default",
+    phone_number text COLLATE pg_catalog."default",
+    account_id uuid NOT NULL,
+    brand_id uuid,
+    outlet_id uuid,
+    source_id uuid NOT NULL,
+    source_type text COLLATE pg_catalog."default" NOT NULL,
+    source_created_at timestamp with time zone NOT NULL,
+    source_updated_at timestamp with time zone NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data jsonb NOT NULL
+);
+
+CREATE INDEX IF NOT EXISTS event_stream_account_id_idx
+    ON public.event_stream USING btree
+    (account_id ASC NULLS LAST)
+    INCLUDE(source_id, source_type)
+    TABLESPACE pg_default;
+-- Index: event_stream_created_at_idx
+
+-- DROP INDEX IF EXISTS public.event_stream_created_at_idx;
+
+CREATE INDEX IF NOT EXISTS event_stream_created_at_idx
+    ON public.event_stream USING btree
+    (created_at ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: event_stream_profile_id_idx
+
+-- DROP INDEX IF EXISTS public.event_stream_profile_id_idx;
+
+CREATE INDEX IF NOT EXISTS event_stream_profile_id_idx
+    ON public.event_stream USING btree
+    (profile_id COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default
+    WHERE profile_id IS NOT NULL;
+-- Index: event_stream_source_type_source_id_uindex
+
+-- DROP INDEX IF EXISTS public.event_stream_source_type_source_id_uindex;
+
+CREATE UNIQUE INDEX IF NOT EXISTS event_stream_source_type_source_id_uindex
+    ON public.event_stream USING btree
+    (source_type COLLATE pg_catalog."default" ASC NULLS LAST, source_id ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_event_stream_custom
+
+-- DROP INDEX IF EXISTS public.idx_event_stream_custom;
+
+CREATE INDEX IF NOT EXISTS idx_event_stream_custom
+    ON public.event_stream USING btree
+    (created_at ASC NULLS LAST, account_id ASC NULLS LAST, source_type COLLATE pg_catalog."default" ASC NULLS LAST, source_id ASC NULLS LAST)
+    TABLESPACE pg_default
+    WHERE source_type = 'CUSTOM_EVENT'::text;
+-- Index: idx_event_stream_non_custom
+
+-- DROP INDEX IF EXISTS public.idx_event_stream_non_custom;
+
+CREATE INDEX IF NOT EXISTS idx_event_stream_non_custom
+    ON public.event_stream USING btree
+    (created_at ASC NULLS LAST, account_id ASC NULLS LAST, source_type COLLATE pg_catalog."default" ASC NULLS LAST)
+    TABLESPACE pg_default
+    WHERE source_type <> 'CUSTOM_EVENT'::text;
+-- Index: idx_source_id
+
+-- DROP INDEX IF EXISTS public.idx_source_id;
+
+CREATE INDEX IF NOT EXISTS idx_source_id
+    ON public.event_stream USING btree
+    (source_id ASC NULLS LAST)
+    TABLESPACE pg_default;
+-- Index: idx_unique_data_hash
+
+-- DROP INDEX IF EXISTS public.idx_unique_data_hash;
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_data_hash
+    ON public.event_stream USING btree
+    ((md5(data::text)::uuid) ASC NULLS LAST)
+    TABLESPACE pg_default;
